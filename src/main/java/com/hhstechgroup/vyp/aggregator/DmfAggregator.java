7ff5f508a2b22bf9b76028e535e50ebbc9dc740f
Bhavesh Furia <bhavesh.furia@cuelogic.com>
2020-06-16 02:45:07 +0530

    DMF in progress

---
+++ src/main/java/com/hhstechgroup/vyp/aggregator/DmfAggregator.java
@@ -0,0 +1,39 @@
+package com.hhstechgroup.vyp.aggregator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+
+import com.hhstechgroup.vyp.model.DeathMaster;
+
+public class DmfAggregator implements AggregationStrategy {
+
+    @Override
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+        // TODO Auto-generated method stub
+        Message newIn = newExchange.getIn();
+        DeathMaster obj = (DeathMaster) newIn.getBody();
+        ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        Map<String, String> m = new HashMap<String, String>();
+        if (oldExchange == null) { // first iteration
+            list = new ArrayList<Map<String, String>>();
+            m.put("id", obj.getLast_name());
+            m.put("name", obj.getFirst_name());
+            list.add(m);
+            newIn.setBody(list);
+            return newExchange;
+        } else { // second iteration onwards
+            Message in = oldExchange.getIn();
+            list = in.getBody(ArrayList.class);
+            m.put("id", obj.getLast_name());
+            m.put("name", obj.getFirst_name());
+            list.add(m);
+            return oldExchange;
+        }
+    }
+
+}
--- src/main/java/com/hhstechgroup/vyp/routes/DeathMasterRouteBuilder.java
+++ src/main/java/com/hhstechgroup/vyp/routes/DeathMasterRouteBuilder.java
@@ -5,40 +5,42 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
 import org.apache.camel.spi.DataFormat;
 
-import com.hhstechgroup.vyp.aggregator.LeieAggregator;
-import com.hhstechgroup.vyp.model.LeieExclusion;
-import com.hhstechgroup.vyp.processor.LeieRecordProcessor;
+import com.hhstechgroup.vyp.aggregator.DmfAggregator;
+import com.hhstechgroup.vyp.model.DeathMaster;
+import com.hhstechgroup.vyp.processor.DmfRecordProcessor;
 import com.hhstechgroup.vyp.utility.Idempotentable;
 
-public class LeieRouteBuilder extends RouteBuilder implements Idempotentable {
+public class DeathMasterRouteBuilder extends RouteBuilder implements Idempotentable {
 
     @Override
     public void configure() throws Exception {
-        final DataFormat bindyObj = new BindyCsvDataFormat(LeieExclusion.class);
         // TODO Auto-generated method stub
-        from("file:/home/cuelogic.local/bhavesh.furia/camel/input/vyp/leie/?noop=true")
-        .routeId("fileMessageFromLeieFolder")
+        final DataFormat bindyObj = new BindyCsvDataFormat(DeathMaster.class);
+        // TODO Auto-generated method stub
+        from("file:/home/cuelogic.local/bhavesh.furia/camel/input/vyp/dmf/?noop=true")
+        .routeId("fileMessageFromDmfFolder")
         .split(body().tokenize("\n"))
         .streaming()
-        .to("direct:individuaLeieRecord");
+        .to("direct:individuaDmfRecord");
 
-        from("direct:individuaLeieRecord")
-        .routeId("individualLeieRowRecord")
+        from("direct:individuaDmfRecord")
+        .routeId("individualDmfRecord")
         .errorHandler(
                 defaultErrorHandler()
                 .redeliveryDelay(2000)
                 .maximumRedeliveries(15)
                 .retryAttemptedLogLevel(LoggingLevel.ERROR)
               )
-        .process(new LeieRecordProcessor())
-        .idempotentConsumer(header("msgHash"), getIdempotentRepository("leie-exclusion-idempotent-repo"))
+        .process(new DmfRecordProcessor())
+        .idempotentConsumer(header("msgHash"), getIdempotentRepository("death-master-idempotent-repo"))
         .log("Processing msg")
         .unmarshal(bindyObj)
-        .aggregate(constant(true), new LeieAggregator())
+        .aggregate(constant(true), new DmfAggregator())
         .completionSize(50)
         .completionTimeout(5000)
 //        .aggregationRepository(getAggregationRepository())
         .to("sql:insert into exclusions(lastname, firstname) values (:#id, :#name)?batch=true")
         .end();
     }
+
 }
