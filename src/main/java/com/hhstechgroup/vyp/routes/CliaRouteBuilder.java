77b42f2b5aeef73ecdd0985eca3121fbafa4dcf2
Bhavesh Furia <bhavesh.furia@cuelogic.com>
2020-07-01 18:00:37 +0530

    error handling - added exponential retry mechanism when connection to DB is failing

--- src/main/java/com/hhstechgroup/vyp/routes/CliaRouteBuilder.java
+++ src/main/java/com/hhstechgroup/vyp/routes/CliaRouteBuilder.java
@@ -1,15 +1,20 @@
 package com.hhstechgroup.vyp.routes;
 
+import java.sql.SQLException;
+
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
 import org.apache.camel.spi.DataFormat;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 import com.hhstechgroup.vyp.aggregator.CliaAggregator;
 import com.hhstechgroup.vyp.aggregator.NpiAggregator;
 import com.hhstechgroup.vyp.model.Clia;
 import com.hhstechgroup.vyp.model.NppesNPI;
 import com.hhstechgroup.vyp.processor.CliaRecordProcessor;
+import com.hhstechgroup.vyp.processor.GenerateFailureResponse;
 import com.hhstechgroup.vyp.processor.NpiRecordProcessor;
 import com.hhstechgroup.vyp.utility.Idempotentable;
 
@@ -21,34 +26,46 @@ public class CliaRouteBuilder extends RouteBuilder implements Idempotentable {
         final String datasource_name = "clia";
 
         // TODO Auto-generated method stub
+        onException(CannotGetJdbcConnectionException.class)
+            .maximumRedeliveries(10)
+            .redeliveryDelay(2000)
+            .useExponentialBackOff();
+
         from("file:camel/input/vyp/"+datasource_name+"/?noop=true")
         .routeId("fileMessageFrom"+datasource_name+"Folder")
         .split(body().tokenize("\n"))
         .streaming()
         .choice()
-        .when(body().contains("CITY_NAME"))
-            .log("Ignoring message because a header row is detected - "+body())
-            .to("direct:trash")
-        .otherwise()
-            .to("direct:individual"+datasource_name+"Record");
+            .when(body().contains("CITY_NAME"))
+                .log("Ignoring message because a header row is detected - "+body())
+                .to("direct:trash")
+            .otherwise()
+                .to("direct:individual"+datasource_name+"Record");
 
         from("direct:individual"+datasource_name+"Record")
         .routeId("individual"+datasource_name+"RowRecord")
-        .errorHandler(
-                defaultErrorHandler()
-                .redeliveryDelay(2000)
-                .maximumRedeliveries(15)
-                .retryAttemptedLogLevel(LoggingLevel.ERROR)
-              )
+//        .errorHandler(
+//                defaultErrorHandler()
+//                .redeliveryDelay(2000)
+//                .maximumRedeliveries(3)
+//                .retryAttemptedLogLevel(LoggingLevel.ERROR)
+//              )
+//        .errorHandler(
+//                deadLetterChannel("direct:dlq")
+//                .useOriginalMessage()
+//                .maximumRedeliveries(3)
+//                .redeliveryDelay(2000)
+//                .retryAttemptedLogLevel(LoggingLevel.ERROR)
+//                )
         .process(new CliaRecordProcessor())
         .idempotentConsumer(header("msgHash"), getIdempotentRepository(datasource_name))
-        .log("Processing msg")
         .unmarshal(bindyObj)
         .aggregate(constant(true), new CliaAggregator())
         .completionSize(50)
         .completionTimeout(5000)
 //        .aggregationRepository(getAggregationRepository())
         .to("sql:insert into clia(prvdr_ctgry_sbtyp_cd, prvdr_ctgry_cd) values (:#id, :#name)?batch=true")
+//      .to("mybatis:insertAccount?statementType=Insert")
         .end();
     }
 }
